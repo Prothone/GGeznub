a5dac45c30568cef75e8d39bebbfb3b0511cc81c
Prothone <flashing.schlei3@gmail.com>
2016-05-15 22:34:50 +0900

    main menu 1

--- app/src/main/java/calluswibu/ggeznub/MainMenu.java
+++ app/src/main/java/calluswibu/ggeznub/MainMenu.java
@@ -1,13 +1,20 @@
 package calluswibu.ggeznub;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
     }
 }
